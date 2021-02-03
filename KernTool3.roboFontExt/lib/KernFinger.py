c18b4d91ad774be50234bf2d9abc6fb9a6b129b6
typedev <lubovenko@me.com>
2021-02-03 18:44:50 +0300

    del

--- KernTool3.roboFontExt/lib/KernFinger.py
+++
@@ -1,8 +0,0 @@
-import tdKernListView
-import importlib
-from fontParts.world import CurrentFont
-importlib.reload(tdKernListView)
-from tdKernListView import TDKernFinger
-# fix layer color for GlyphCollectionView
-# CurrentFont().getLayer('foreground').color = (0, 0, 0, 1)
-TDKernFinger()
--- KernTool3.roboFontExt/lib/KernTool3.py
+++
@@ -1,8 +0,0 @@
-import tdKernObserver
-import importlib
-from fontParts.world import CurrentFont
-importlib.reload(tdKernObserver)
-from tdKernObserver import KernObserver
-# fix layer color for GlyphCollectionView
-# CurrentFont().getLayer('foreground').color = (0, 0, 0, 1)
-KernObserver()
